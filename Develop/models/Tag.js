6cd9952ec030357b5e577e673bd2b36c6b14b5ed
kevinkorza <kevingkorza@gmail.com>
2022-09-30 14:30:40 -0400

    models completed

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -4,9 +4,23 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
+//define columns
+//Id, Integer, Doesn't allow null values, Set as primary key, uses auto increment, tag_name, String
+
+
 Tag.init(
   {
-    // define columns
+    //id
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //tag name
+    tag_name: {
+        type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
