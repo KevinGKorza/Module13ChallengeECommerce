b642468bc4500e5418867275e00368e609cbb3fa
kevinkorza <kevingkorza@gmail.com>
2022-10-03 14:55:21 -0400

    category routes and tested in insomnia, created a env file

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,15 +6,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +26,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.update
 });
 
 module.exports = router;
