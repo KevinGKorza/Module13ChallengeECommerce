ec419fc31287dc72adc737caf661f7c0cc9eae3f
kevinkorza <kevingkorza@gmail.com>
2022-10-04 14:20:29 -0400

    product routes update

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -37,7 +37,33 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+      },
+    ],
+  })
+  .then(categoryData => {
+    if(!categoryData) {
+      res.status(404).json({message: "Unable to find ID!"});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
